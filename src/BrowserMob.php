5894abf5cf015d96127443a84e157b04cc477562
Greg Heitz <greg.heitz@userreplay.com>
2016-07-07 22:16:38 +0100

    Add methods and tests (WIP)

--- src/BrowserMob.php
+++ src/BrowserMob.php
@@ -10,7 +10,7 @@ use \Requests;
 class BrowserMob extends Module
 {
 
-    protected $config = ['host', 'port', 'blacklist', 'whitelist', 'limits', 'timeouts', 'redirect', 'retry'];
+    protected $config = ['host', 'port', 'autostart', 'blacklist', 'whitelist', 'limits', 'timeouts', 'redirect', 'retry', 'basicAuth'];
 
     protected $requiredFields = ['host'];
 
@@ -37,11 +37,11 @@ class BrowserMob extends Module
         }
 
         // start a new BrowserMobProxy session
-        $this->bmp->open();
-
-        // set BrowserMobProxy options
-        $this->__setProxyCapabilities();
-
+        if (isset($this->config['autostart'])) {
+            if (true === (bool)$this->config['autostart']) {
+                $this->openProxy();
+            }
+        }
     }
 
     protected static function __pingProxy($url)
@@ -55,33 +55,36 @@ class BrowserMob extends Module
         return $response->success;
     }
 
-    protected function __setProxyCapabilities()
+    protected function __setProxyCapabilities($capabilities)
     {
         $response = null;
 
-        foreach ($this->config as $config => $data) {
+        foreach ($capabilities as $config => $data) {
             try {
                 if (false === empty($data)) {
                     switch ($config) {
                         case 0: // fix a weird PHP behaviour: when $config === 0 then go in 'blacklist'
                             break;
                         case 'blacklist':
-                            $response = $this->bmp->blacklist($data);
+                            $response = $this-_blacklist($data);
                             break;
                         case 'whitelist':
-                            $response = $this->bmp->whitelist($data);
+                            $response = $this->_whitelist($data);
                             break;
                         case 'limits':
-                            $response = $this->bmp->limits($data);
+                            $response = $this->_limits($data);
                             break;
                         case 'timeouts':
-                            $response = $this->bmp->timeouts($data);
+                            $response = $this->_timeouts($data);
                             break;
                         case 'redirect':
-                            $response = $this->bmp->remapHosts($data);
+                            $response = $this->_remapHosts($data);
                             break;
                         case 'retry':
-                            $response = $this->bmp->retry($data);
+                            $response = $this->_retry($data);
+                            break;
+                        case 'basicAuth':
+                            $response = $this->_basicAuth($data);
                             break;
                         default:
                             // do nothing
@@ -97,6 +100,86 @@ class BrowserMob extends Module
                     throw new ModuleConfigException(__CLASS__, "Proxy response error '{$reponse->status_code}' {$respone->body}");
                 }
             }
+        }
+    }
+
+    public function openProxy($capabilities = null)
+    {
+        try {
+            $this->bmp->open();
+            if (empty($capabilities)) {
+                $capabilities = $this->config;
+            }
+            $this->__setProxyCapabilities($capabilities);
+        } catch(\Exception $e) {
+            throw new ModuleException(__CLASS__, $e->getMessage());
+        }
+    }
+
+    public function closeProxy()
+    {
+        try {
+            $this->bmp->close();
+        } catch(\Exception $e) {
+            throw new ModuleException(__CLASS__, $e->getMessage());
+        }
+    }
+
+    public function startHar()
+    {
+        try {
+            $response = $this->bmp->newHar();
+        } catch(\Exception $e) {
+            throw new ModuleException(__CLASS__, $e->getMessage());
+        }
+        return $response->success;
+    }
+
+    public function startPage()
+    {
+        try {
+            $response = $this->bmp->newPage();
+        } catch(\Exception $e) {
+            throw new ModuleException(__CLASS__, $e->getMessage());
+        }
+        return $response->success;
+    }
+
+    public function getHar()
+    {
+        try {
+            return $this->bmp->har;
+        } catch(\Exception $e) {
+            throw new ModuleException(__CLASS__, $e->getMessage());
+        }
+    }
+
+    public function getLimit()
+    {
+        try {
+            $har = $this->bmp->getLimit();
+        } catch(\Exception $e) {
+            throw new ModuleException(__CLASS__, $e->getMessage());
+        }
+    }
+
+    // magic function giving direct access to BrowserMobProxy class methods
+    public function __call($method, $args)
+    {
+        // check if is a command call
+        if (preg_match('/^_[A-z]+$/', $method)) {
+            // extract standard method name
+            $method = preg_filter('/_/', '', $method);
+            // set call array for calling method
+            $call = array($this, $method);
+            // check if method is callable
+            if (is_callable($call)) {
+                call_user_func_array($call, $args);
+            } else {
+                throw new RuntimeException("Method ${method} does not exist or is not callable");
+            }
+        } else {
+            throw new RuntimeException("Method ${method} does not exist or is not callable");
         }
     }
 
--- tests/functional/BrowserMoxProxyCest.php
+++ tests/functional/BrowserMoxProxyCest.php
@@ -10,4 +10,13 @@ class BrowserMobProxyCest
         $module = new BrowserMob(Stub::make('Codeception\Lib\ModuleContainer'));
         $I->assertInstanceOf('Codeception\Extension\BrowserMob', $module);
     }
+
+    public function getHar(FunctionalTester $I)
+    {
+        $I->openProxy();
+        $I->startHar();
+        Requests::get('http://www.github.com', [], ['proxy' => '127.0.0.1:9090']);
+        $I->assertNotNull($I->getHar());
+
+    }
 }
